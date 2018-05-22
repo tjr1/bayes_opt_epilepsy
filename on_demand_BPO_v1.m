4dfc6f713016edcd0b5f2685e1f0f30f833173dd
Thomas Richner <tjr1@users.noreply.github.com>
2018-05-22 10:45:26 -0500

    fixed a missing creatfcn

--- on_demand_BPO_v1.m
+++ on_demand_BPO_v1.m
@@ -104,6 +104,18 @@ if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgr
 end
 
 % --- Executes during object creation, after setting all properties.
+function ET_min_width_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to ET_load_path (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+% --- Executes during object creation, after setting all properties.
 function ET_min_spikes_per_two_s_CreateFcn(hObject, eventdata, handles)
 % hObject    handle to ET_load_path (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
